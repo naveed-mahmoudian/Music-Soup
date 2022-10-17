5668e7c9c41423809d647e16fc7e383abc9528eb
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-10-16 22:38:37 -0700

    Working on keyowrd API

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,10 @@
 const getStartedBtn = document.querySelector("#getStartedBtn");
 const clientID = "805b4589fb474d7f8007a468b5879903";
 const clientSecret = "eadbd8eabea14e02a4c463fba46c6fc0";
+const render = document.getElementById("render");
+let storedToken;
+let searchBtn;
+const apiKey = "EsUEPgZZeR0POnwasZa8al2WpaC3QR90NUVgfiYUKKw";
 
 const getToken = () => {
   fetch("https://accounts.spotify.com/api/token", {
@@ -16,9 +20,50 @@ const getToken = () => {
     .catch((err) => console.log(err));
 };
 
-getStartedBtn.addEventListener("click", getToken);
-
 function storeToken(token) {
-  const storedToken = token;
-  console.log(storedToken);
+  storedToken = localStorage.setItem("storedToken", token);
+}
+
+getStartedBtn.addEventListener("click", showForm);
+
+function showForm() {
+  render.classList.add("hideAnim");
+  render.addEventListener("transitionend", function (event) {
+    if (event.propertyName !== "opacity") return;
+    render.classList.remove("hideAnim");
+    // render.classList.add("hidden");
+
+    render.innerHTML = "";
+    render.innerHTML = `<div class="col">
+    <div class="text-white fs-1">Paste Your Brief</div>
+    <div class="input-group">
+      <textarea class="form-control" id="text"></textarea>
+    </div> 
+    <button type="button" id="searchBtn" class="btn btn-dark m-3">
+      Search
+    </button>
+  </div>`;
+
+    render.classList.add("showAnim");
+
+    searchBtn = document.getElementById("searchBtn");
+    searchBtn.addEventListener("click", findKeywords);
+  });
+}
+
+function findKeywords() {
+  const text = document.getElementById("text").value;
+  console.log(text);
+  fetch(
+    "https://apis.paralleldots.com/v4/keywords?text=" +
+      text +
+      "&api_key=" +
+      apiKey,
+    {
+      method: "POST",
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
 }
