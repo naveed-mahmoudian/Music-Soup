efdfd430cda9014a364b37fd6d1e2a483a287fd6
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-12-06 12:23:41 -0800

    converting to MVC

--- public/index.js
+++ public/index.js
@@ -1,2 +1,2 @@
-const getStartedBtn = document.getElementById("getStartedBtn");
+
 
--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 // Import Express
 const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+require("dotenv").config();
+
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
+// Handlebars
+const hbs = exphbs.create();
+
+// Session
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 // Set up middleware
 app.use(express.static("public"));
